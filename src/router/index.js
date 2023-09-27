095d28bbc158f68a016433d9dd2da25e0715fd63
selfarion333 <144839864+selfarion333@users.noreply.github.com>
2023-09-27 13:42:53 -0500

    Add files via upload

--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,14 @@ import comprasAll from '../components/compras_com.vue'
 import ventasAll from '../components/ventas_com.vue'
 import cli_1 from '../components/clientes.vue'
 import login_1 from '../components/login.vue'
+import tickets from '../components/Tikets.vue'
 
 const routes=[
     {path:"/compras", name:"compras", component: comprasAll,},
     {path:"/Ventas", name:"ventas", component: ventasAll,},
     {path:"/login",name:"login", component:login_1},
     {path:"/Clientes", name:"clientes", component: cli_1},
+    {path:"/tickets", name:"tikets", component:tickets}
 ]
 const router=createRouter({
     history:createWebHistory(),routes,
