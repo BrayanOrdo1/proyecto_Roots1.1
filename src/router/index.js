443511810e1a52bb49cbd7e71e0554947e183686
selfarion333 <144839864+selfarion333@users.noreply.github.com>
2023-09-27 17:39:49 -0500

    Add files via upload

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,29 @@
 import { createRouter, createWebHistory } from "vue-router";
-import comprasAll from '../components/compras_com.vue'
+import home from '../components/home.vue'
 import ventasAll from '../components/ventas_com.vue'
 import cli_1 from '../components/clientes.vue'
 import login_1 from '../components/login.vue'
 import tickets from '../components/Tikets.vue'
 
+import store from '../stores/store'
+
 const routes=[
-    {path:"/compras", name:"compras", component: comprasAll,},
-    {path:"/Ventas", name:"ventas", component: ventasAll,},
-    {path:"/login",name:"login", component:login_1},
-    {path:"/Clientes", name:"clientes", component: cli_1},
-    {path:"/tickets", name:"tikets", component:tickets}
+    {path:"/Home", name:"home", component: home,meta:{requireAuth:false}},
+    {path:"/Ventas", name:"ventas", component: ventasAll,meta:{requireAuth:true}},
+    {path:"/login",name:"login", component:login_1,meta:{requireAuth:false}},
+    {path:"/Clientes", name:"clientes", component: cli_1,meta:{requireAuth:true}},
+    {path:"/tickets", name:"tikets", component:tickets,meta:{requireAuth:true}}
 ]
 const router=createRouter({
     history:createWebHistory(),routes,
+});
+
+router.beforeEach((to,from,next) => {
+    if (to.meta.requireAuth && !store.state.isAuthentificated) {
+        next('/home');
+    } else {
+        next()
+    }
 })
+
 export default router;
