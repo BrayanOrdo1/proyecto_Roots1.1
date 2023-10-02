ade23f7ac76e8c920166b9ad5f7c2218fc16584c
BrayanOrdo1 <132753791+BrayanOrdo1@users.noreply.github.com>
2023-10-02 17:45:58 -0500

    Add files via upload

---
+++ Nomina.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="Megadiv">
+       
+    </div>
+
+</template>
+<script>
+
+</script>
+<style>
+.Megadiv{
+    height: 92.6vh; 
+    width: 100vw;
+    background-color: black;
+}
+</style>
