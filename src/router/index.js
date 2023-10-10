55793ee9712d5ce73a5da2aff51596f86f1fc5ed
selfarion333 <144839864+selfarion333@users.noreply.github.com>
2023-10-10 16:44:38 -0500

    Add files via upload

--- src/router/index.js
+++ src/router/index.js
@@ -7,26 +7,31 @@ import tickets from '../components/Tikets.vue'
 import usuario from '../components/Usuario_C.vue'
 import Nomina from '../components/Nomina.vue'
 
+import store from '../stores/store';
 
 const routes=[
-    {path:"/Home", name:"home", component: home,},
-    {path:"/Ventas", name:"ventas", component: ventasAll,},
-    {path:"/login",name:"login", component:login_1,},
-    {path:"/Clientes", name:"clientes", component: cli_1,},
-    {path:"/tickets", name:"tikets", component:tickets,},
-    {path:"/usuario", name:"usuario", component:usuario,},
-    {path:"/Nomina", name:"nomina", component:Nomina,}
+    {path:"/Home", name:"home", component: home,meta: { requiresAuth: false }},
+    {path:"/Ventas", name:"ventas", component: ventasAll,meta: { requiresAuth: true }},
+    {path:"/login",name:"login", component:login_1,meta: { requiresAuth: false }},
+    {path:"/Clientes", name:"clientes", component: cli_1,meta: { requiresAuth: true }},
+    {path:"/tickets", name:"tikets", component:tickets,meta: { requiresAuth: true }},
+    {path:"/usuario", name:"usuario", component:usuario,meta: { requiresAuth: true }},
+    {path:"/Nomina", name:"nomina", component:Nomina,meta: { requiresAuth: true }}
 ]
 const router=createRouter({
     history:createWebHistory(),routes,
 });
 
-// router.beforeEach((to,from,next) => {
-//     if (to.meta.requireAuth && !store.state.isAuthentificated) {
-//         next('/ome');
-//     } else {
-//         next()
-//     }
-// })
+router.beforeEach((to, from, next) => {
 
+    // Comprobar si la ruta requiere autenticación
+    if (to.matched.some(record => record.meta.requiresAuth)) {
+        if (!store.state.isAuthenticated) {
+            next('/home');  // Redirecciona al login
+            return;
+        }
+    }
+    
+    next();  // Continúa a la ruta solicitada
+});
 export default router;
