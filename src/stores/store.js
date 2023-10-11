3b078f61196eab66e9e4c00683d0f4a4efe21feb
selfarion333 <144839864+selfarion333@users.noreply.github.com>
2023-10-10 22:17:19 -0500

    Add files via upload

--- src/stores/store.js
+++ src/stores/store.js
@@ -1,48 +1,54 @@
+// store.js
 import { createStore } from 'vuex'
 
 export default createStore({
     state: {
-        isAuthentificated: false,
-        userData: null
+        isAuthenticated: false,
+        userData: null,
     },
 
-    mutations:{
-        SET_USER(state,user) {
-            state.isAuthentificated=true;
-            state.userData=user;
+    mutations: {
+        SET_USER(state, user) {
+            state.isAuthenticated = true;
+            state.userData = user;
             localStorage.setItem('user', JSON.stringify(user));
         },
         LOGOUT(state) {
-            state.isAuthentificated=false;
-            state.userData=null;
+            state.isAuthenticated = false;
+            state.userData = null;
             localStorage.removeItem('user');
             localStorage.removeItem('userId');
         },
         limpiarCredenciales(state) {
-            state.isAuthentificated=false;
-            state.userData=null;
-            localStorage.removeItem('user')
+            // Lógica para limpiar las credenciales
+            state.isAuthenticated = false;
+            state.userData = null;
+            localStorage.removeItem('user');
         },
-        setUserAuthenticated(state,value) {
-            state.isAuthentificated=value;
-        }
+        setUserAuthenticated(state, value) {
+            state.isAuthenticated = value;
+        },
     },
 
     actions: {
-        setUser({commit },user) {
+        setUser({ commit }, user) {
             commit('SET_USER', user);
         },
-        logout({commit}) {
+        logout({ commit }) {
             commit('LOGOUT');
         },
-        initializeStore({state}) {
+        initializeStore({ state }) {
             if (localStorage.getItem('user')) {
-                state.userData=JSON.parse(localStorage.getItem('user'));
-                state.isAuthentificated=true
+                state.userData = JSON.parse(localStorage.getItem('user'));
+                state.isAuthenticated = true;
             } else {
-                state.userData=null;
-                state.isAuthentificated=false                
+                state.userData = null;
+                state.isAuthenticated = false;
             }
         }
     }
-})
+});
+
+
+
+
